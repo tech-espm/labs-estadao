ea85a3b0a3a9631457e625a7371153dba5064f70
Pedro Miotti <pedromiotti7@gmail.com>
2020-05-09 12:13:17 -0300

    Added file upload on the Front-End

--- routes/api/usuario.ts
+++ routes/api/usuario.ts
@@ -6,7 +6,7 @@ import Usuario = require("../../models/usuario");
 const router = express.Router();
 
 // Se utilizar router.xxx() mas não utilizar o wrap(), as exceções ocorridas
-// dentro da função async não serão tratadas!!!
+// dentro da função async não serão tratadas!!! 
 router.post("/alterarPerfil", wrap(async (req: express.Request, res: express.Response) => {
 	let u = await Usuario.cookie(req, res);
 	if (!u)
