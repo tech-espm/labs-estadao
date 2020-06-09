aebd936e2bbef573cb7e50db543051111aa67172
LeonardoNunes-N <58406267+LeonardoNunes-N@users.noreply.github.com>
2020-06-09 16:14:52 -0300

    Add files via upload

--- routes/quiz.ts
+++ routes/quiz.ts
@@ -5,7 +5,7 @@ import appsettings = require("../appsettings");
 import Usuario = require("../models/usuario");
 import Tipo = require("../models/quiz/Tipo")
 import Pergunta = require("../models/quiz/pergunta");
-
+import Quiz = require("../models/quiz/quiz");
 
 const router = express.Router();
 
@@ -47,6 +47,14 @@ router.get("/editarPerg", wrap(async (req: express.Request, res: express.Respons
 		res.redirect(appsettings.root + "/login");
 	else
 		res.render("quiz/editarPerg", { titulo: "Editar Pergunta", usuario: u, quiz_id: req.query.quiz_id, pergunta: await Pergunta.obter(parseInt(req.query.quiz_id), parseInt(req.query.perg_id)) });
+})); 
+
+router.get("/listar", wrap(async (req: express.Request, res: express.Response) => {
+	let u = await Usuario.cookie(req);
+	if (!u || !u.admin)
+		res.redirect(appsettings.root + "/acesso");
+	else
+		res.render("quiz/listar", { titulo: "Gerenciar Quiz", usuario: u, lista: JSON.stringify(await Quiz.listar()) });
 }));
 
 export = router;
